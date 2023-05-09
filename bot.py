e7e5121adef1805e17ebc7b4f079f80a9a781222
Joseph DeGregorio <joseeph.degregorio@gmail.com>
2023-05-09 02:03:06 -0700

    Final tuning of the insult prompt and functionality

--- bot.py
+++ bot.py
@@ -9,12 +9,11 @@ from llm import insult_jim
 # Load environment variables
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-EMOJI = '😋'
+EMOJI = '🏈' 
+DEFAULT_MESSAGE = "Hello! My chat functionality is still under development, but I can help you insult Jim if you want, just type `!insultjim` into a new message and I'll get right on it!"
 
 # Specify intents
 intents = discord.Intents.all()
-# intents.messages = True
-# intents.guilds = True
 
 # Initialize bot
 bot = commands.Bot(command_prefix='!', intents=intents)
@@ -23,8 +22,6 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 async def on_ready():
     """Print bot information when connected to Discord."""
     print(f'{bot.user.name} has connected to Discord!')
-    my_loop.start()
-    my_scheduled_task.start()
 
 # Send message after a message is sent
 @bot.event
@@ -37,18 +34,7 @@ async def on_message(message):
         return
 
     if bot.user in message.mentions or "🤖" in message.content:
-        # last_n_messages = []
-        # async for msg in message.channel.history(limit=5):  # Change the limit to desired N value
-        #     if msg.id != message.id:
-        #         last_n_messages.append(msg)
-
-        # reply_text = f"Here are the last {len(last_n_messages)} messages:\n"
-        # for msg in reversed(last_n_messages):
-        #     reply_text += f"{msg.author.name} said this: {msg.content}\n"
-
-        # await message.reply(reply_text)
-        await message.reply('Hello! I hear you quiet and clear!')
-
+        await message.reply(DEFAULT_MESSAGE)
     await bot.process_commands(message)
 
 # Send message after a specific emoji reaction is added to a message
@@ -60,62 +46,14 @@ async def on_raw_reaction_add(payload):
     if payload.emoji.name == EMOJI:
         channel = bot.get_channel(payload.channel_id)
         message = await channel.fetch_message(payload.message_id)
-        response = f'{payload.member.mention} reacted with {EMOJI} to the message:\n> {message.content}\n> —{message.author.mention}'
+        # response = f'{payload.member.mention} reacted with {EMOJI} to the message:\n> {message.content}\n> —{message.author.mention}'
+        response = "Go football! Woo"
         await channel.send(response)
 
-
-# Perform task at specific times (UTC)
-times = [
-    datetime.time(hour=15, minute=0, second=0)
-]
-@tasks.loop(time=times)
-async def my_scheduled_task():
-    print("My scheduled task is running!")
-    channel = bot.get_channel(int('1105232705232322680'))
-    await channel.send('This is your scheduled message!')
-
-# Perform tasks on a loop
-@tasks.loop(seconds=15)
-async def my_loop():
-    print('My looped message is running')
-    channel = bot.get_channel(int('1105232705232322680'))
+@bot.command(name='insultjim')
+async def _insult_jim(ctx):
+    """Generate and send an insult for Jim."""
     insult = insult_jim()
-    await channel.send(insult)
-
-# async def openai_chat_reply(system_message, discord_messages):
-#     """
-#     Generate a response using OpenAI's Chat API given a custom system message and recent Discord messages.
-#     :param system_message: The custom system message to set the behavior of the assistant.
-#     :param discord_messages: The list of recent Discord messages to provide context for the assistant.
-#     :return: The assistant's response as a string.
-#     """
-#     messages = [{"role": "system", "content": system_message}]
-#     for msg in discord_messages:
-#         messages.append({"role": "user", "content": msg.content})
-
-#     response = openai.ChatCompletion.create(
-#         model="gpt-3.5-turbo",
-#         messages=messages
-#     )
-
-#     reply = response['choices'][0]['message']['content']
-#     return reply
-
-
-# @bot.event
-# async def on_message(message):
-#     if message.author == bot.user:
-#         return
-
-#     if bot.user in message.mentions or "🤖" in message.content:
-#         last_n_messages = []
-#         async for msg in message.channel.history(limit=5):  # Change the limit to desired N value
-#             if msg.id != message.id:
-#                 last_n_messages.append(msg)
-
-#         system_message = "You are a helpful assistant."
-#         reply = await openai_chat_reply(system_message, last_n_messages)
-#         await message.reply(reply)
-
+    await ctx.send(insult)
 
 bot.run(TOKEN)
--- llm.py
+++ llm.py
@@ -2,14 +2,43 @@ from langchain.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
 from prompt import PROMPT_INSULT
 from dotenv import load_dotenv
+import random
 
 # Load environment variables
 load_dotenv()
 
-def insult_jim(temperature=0.9):
+def insult_jim(type=None, temperature=None):
+    
+    # Randomly select temperature
+    if temperature is None:
+        temperature = round(random.uniform(0.7, 1.1), 3)
+
+    if type is None:
+        type = random.choice(["joke", "insult", "insulting pun", "witty one-liner insult", "insulting trolling comment"])
+
+    # Create LLM Chain
     chain = LLMChain(
         llm=ChatOpenAI(model='gpt-3.5-turbo', temperature=temperature, max_tokens=1000),
         prompt=PROMPT_INSULT
     )
-    insult = chain("")['text']
+    insult = chain({"dummy": "", "type": type})['text']
     return insult
+
+
+if __name__ == "__main__":
+
+    # Testing
+    for temp in [0.7, 0.8, 0.9, 1.0]:
+        print(f"\Temperature: {temp}\n")
+        for type in ["joke", "insult", "insulting pun", "sarcastic remark", "backhanded compliment", "witty one-liner insult", "insulting trolling comment"]:
+            print(f"Type: {type}")
+            print(insult_jim(type=type, temperature=temp))
+            print("\n")
+
+
+
+# 0.9: "Jim, you're like the Cleveland Browns of fantasy football managers - lots of hype, but in the end, always disappointing."
+
+# 0.6: "Hey Jim, I heard your fantasy football team is like a toddler trying to walk for the first time - all wobbly and constantly falling down. But don't worry, we still love you, even if your team is the equivalent of a baby giraffe on ice skates."
+
+# 0.7: "Hey Jim, I heard that your fantasy team is about as successful as your attempts at growing a full beard. Maybe you should stick to managing your eyebrows instead. #Burn #WillCarterLOL"
