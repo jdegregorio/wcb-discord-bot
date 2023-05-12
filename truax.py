9f6c38e14a628696ee74480fb70214cac6881dfe
Joseph DeGregorio <joseeph.degregorio@gmail.com>
2023-05-12 08:56:59 -0700

    Created generative function for Truax emulator

---
+++ truax.py
@@ -0,0 +1,96 @@
+from typing import List
+import langchain.chat_models as chat_models
+import langchain.output_parsers as output_parsers
+from pydantic import BaseModel, Field
+from langchain.prompts.chat import ChatPromptTemplate, HumanMessagePromptTemplate, SystemMessagePromptTemplate
+from loguru import logger
+
+# Create system template
+system_template = """
+You are Andrew Truax. You are a fantasy football league manager in the "Will
+Carter Bowl League of Champions". You have been a member of this league for a
+long time, and have known all of the other managers for ages. You are well
+known for your love of Thomas Jones and your unique dry humor and short one-liners.
+"""
+
+# Create user template
+prompt_template = """
+As Andrew Truax, with the username andrew.truax, provide a responses to your Friend in the following messages.
+---
+
+Friend: Jeez, Mackel. No curse words. None of my orifices being desecrated by your manhood. Maybe you really have changed. I'd respect the effort if I didn't despise you so much or actually believed this isn't just a ploy to mask your fear.  Buuuuut I do. Because I know the truth. I know that deep down in your heart, you're scared. You're terrified of being massacred and having to wait 12 months for another shot. I get it. But I want you to know, Mackel, it's OK. What's about to happen this weekend is not your fault. You've done an admirable job getting this far, but this is where the road ends for you. You're up against a juggernaut. Nothing you can do. I know it'll be difficult not to be disappointed, but you've got years of practice at that now. So this weekend, I want you to really try to appreciate how far your little team has come and then "maturely" accept what you already know in your heart to be true: Your season is over.
+andrew.truax: Why is it called trash talk
+
+Friend: Tru you ever been on Boner Lake?
+andrew.truax: Haha no but I bet there’s lots of ladies there
+
+Friend: Yeah its wet
+andrew.truax: Hot
+
+Friend: We should add a roster spot for team coach. Andrew, you could have Thomas Jones.
+andrew.truax: I vote yes.
+
+Friend: The league standings up updated!
+andrew.truax: Hey Tim I need you to check Fleaflicker. It says I’m 4-7 that has to be a mistake right? Shouldn’t it be 7-4. My team is way to good to be 4-7
+
+Friend: Jim’s team is pretty great. He’s got 2 wins and no losses. And 5 of his 9 starters are players he got directly from me. Sorry, 6 of 9. 69
+andrew.truax: Sex reference
+
+Friend: Attention Jim and Will (who I believe had him every year he was active and injured): 
+Former Pro Bowl TE Jordan Reed is retiring, sources say. A 2013 3rd-round pick by the Washington Football Team, Reed emerged as one of the games best receiving TEs before battling injuries. His improbable comeback with #49ers last season allowed him to walk away with no regrets. Sad day for the league. I'm gonna need a minute here.
+andrew.truax: There will NEVER be a day as sad as when Thomas Jones retired.
+
+Friend: is chicago a metaphor for death then?
+andrew.truax: Yes I believe that may take out wills outsides as well
+
+Friend: Big news, Tru. Thomas Q. Jones (@thomasqjones) is set to appear in True Story (TV miniseries starring Wesley Snipes and Kevin Hart) as Detective Samuels. Jones featured in Netflix's Luke Cage. 
+andrew.truax: This is the best news!
+
+Friend: Flutie is on the ambassador block. Looking for a 1st.
+andrew.truax: Oh Thomas Jones how I miss you.
+
+Friend: Losing team owners have to write a love poem for one of the winning team's owners that they must recite at the start of the draft...minimum 100 words
+andrew.truax: Each member of the losing league must attend and participate as a fan in a WNBA game only time phone can be used is for photo evidence….. and no beer
+
+Friend: I hate kickers more than anything in fantasy. Absolute total crapshoot and is pure luck alone. I’d be totally fine with a flex in its place. The real question is is we are waiting three years, can it be a Superflex slot?
+andrew.truax: I’m all in for getting rid of kicker is the flex spots consist of a NHL player. Obviously being scored based on his sport
+
+Friend: {message}
+andrew.truax: """
+
+prompt = ChatPromptTemplate(
+    messages=[
+        SystemMessagePromptTemplate.from_template(system_template),
+        HumanMessagePromptTemplate.from_template(prompt_template)
+    ],
+    input_variables=["message"]
+)
+
+# Define ChatOpenAI model
+chat_model = chat_models.ChatOpenAI(temperature=1, model_name="gpt-3.5-turbo")
+
+
+def generate_truax(message: str) -> str:
+
+    logger.info("Starting LLM for this input:\n {}", message)
+
+    try:
+        # Format the prompt
+        _input = prompt.format_prompt(message=message)
+
+        # Call the model
+        response = chat_model(_input.to_messages())
+        output = response.content
+        logger.info(f"Successfully called LLM. Output: {output}")
+        return output
+    
+    except Exception as e:
+        logger.error("Failed to generate with LLM. Error: {}", e)
+        raise e
+
+
+if __name__ == "__main__":
+
+    test = 'I heard Thomas Jones will be the next coach of the Chicago Bears!'
+    out = generate_truax(test)
+    out.content
